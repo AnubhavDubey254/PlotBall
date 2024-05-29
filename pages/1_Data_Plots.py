748502ff4ad39dee410ac455b45626ff841dbace
Anubhav Dubey <153996218+AnubhavDubey254@users.noreply.github.com>
2024-05-29 12:49:24 +0530

    Update 1_Data_Plots.py

--- pages/1_Data_Plots.py
+++ pages/1_Data_Plots.py
@@ -147,7 +147,7 @@ def pass_map(Match_ID,P_ID):
     P.scatter(x = edfb['x_start'],y = edfb['y_start'],ax = ax,c = 'b')
     P.lines(xstart = edfb['x_start'],ystart = edfb['y_start'],xend = edfb['x_end'],yend = edfb['y_end'],ax = ax,color = 'g')
    
-    P.scatter(x = edff['x_start'],y = edff['y_start'],ax = ax,c = 'b',,marker = 'A')
+    P.scatter(x = edff['x_start'],y = edff['y_start'],ax = ax,c = 'b',marker = 'A')
     P.lines(xstart = edff['x_start'],ystart = edff['y_start'],xend = edff['x_end'],yend = edff['y_end'],ax = ax,color = 'r')
 
     P.scatter(x = assists['x_start'],y = assists['y_start'],ax = ax,c = 'b')
