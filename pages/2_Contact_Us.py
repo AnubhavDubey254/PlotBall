f7af6122d076526214df68deeaad3ac0fc40814a
Anubhav Dubey <153996218+AnubhavDubey254@users.noreply.github.com>
2024-05-29 11:08:22 +0530

    Update 2_Contact_Us.py
    Added Github repo link

--- pages/2_Contact_Us.py
+++ pages/2_Contact_Us.py
@@ -3,5 +3,6 @@ import streamlit as st
 st.title('Contact Me')
 st.page_link("https://www.linkedin.com/in/anubhav-dubey-842a7b2ab",label = "Linkedin",icon="🔗")
 st.page_link("https://www.instagram.com/_anubhavv_0/",label = "Instagram",icon="🔗")
+st.page_link("https://github.com/AnubhavDubey254/PlotBall/",label = "Instagram",icon="🔗")
 
 
